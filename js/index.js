2d9920268db0533f8b5884b9afa17af2d764df20
Maximiliano Vidal <vidal.maximiliano@gmail.com>
2025-08-07 19:28:52 -0300

    Funciona carga de cards en main.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,78 @@
-const URL_JSON = ''
+document.addEventListener("DOMContentLoaded", function() {
+
+    const URL_JSON = 'https://raw.githubusercontent.com/Arthur92uy/tcg-safari/refs/heads/main/js/usuarios.json'
+
+    const contenedorDeCards = document.querySelector(".main__container__cards")
+    // --------------------------------------------------------------------------- //
+
+    async function inicializarUsuarios(){
+        const usuariosLocal = localStorage.getItem('usuarios');
+        if(!usuariosLocal) {
+            try {
+                const res = await fetch(URL_JSON);
+                const data = await res.json();
+                localStorage.setItem('usuarios', JSON.stringify(data));
+                console.log('Usuarios iniciales cargados en localstorage');
+            } catch(error){
+            console.error('Error al cargar usuarios', error);
+            }
+        } else {
+            console.log('Usuarios ya existen en localsorage')
+        }
+    }
+    // --------------------------------------------------------------------------- //
+
+    function cargarUsuariosEnCards(contenedor){
+        const usuariosLocal = JSON.parse(localStorage.getItem('usuarios'))
+
+        for (let i=0; i < usuariosLocal.length; i++){
+            if(usuariosLocal[i].eliminado === false) {
+                let usuario = usuariosLocal[i]
+                contenedor.innerHTML += `
+                <div class="main__cards">
+                    <div class="card__options">
+                        <div class="card__initials">CL</div>
+                        <div class="card__icons">
+                            <button type="button" title="ver">
+                                <img src="./img/ojo.png" alt="">
+                            </button>
+                            <button type="button" title="editar">
+                                <img src="./img/lapiz-de-color.png" alt="">
+                            </button>
+                            <button type="button" title="eliminar">
+                                <img src="./img/tacho-de-reciclaje.png" alt="">
+                            </button>
+                        </div>
+                    </div>
+                    <h3 class="card__user-name">${usuario.nombre} ${usuario.apellido}</h3>
+                    <p class="card__user-mail">${usuario.mail}</p>
+                    <div class="card__user-info">
+                        <span class="card__user-rol">${usuario.rol}</span>
+                        <span class="card__user-status">${usuario.estado}</span>
+                    </div>
+                    <div class="card__user-dates">
+                        <p class="card__user-create">Creado: ${usuario.fechaCreacion}</p>
+                        <p class="card__user-last-access">Ultimo acceso: ${usuario.ultimoAcceso}</p>
+                    </div>
+                </div>`
+            }
+        }
+    }
+    // --------------------------------------------------------------------------- //
+
+    function actualizarAvatarCuenta() {
+        const nombre = document.querySelector(".header__user-info p");
+        const rol = document.querySelector(".header__user-subtitle")
+        const usuario = JSON.parse(sessionStorage.getItem("usuarioActivo")) || {};
+
+        if (nombre && usuario.nombre && usuario.apellido && usuario.rol) {
+            nombre.innerText = usuario.nombre + " " + usuario.apellido;
+            rol.innerText = usuario.rol
+        }
+    }
+    // --------------------------------------------------------------------------- //
+
+    inicializarUsuarios()
+    actualizarAvatarCuenta()
+    cargarUsuariosEnCards(contenedorDeCards)
+})
