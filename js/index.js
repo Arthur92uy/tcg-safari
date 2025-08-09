58e9d8d96d02fd621a21371f9a454c3a321148c9
Maximiliano Vidal <vidal.maximiliano@gmail.com>
2025-08-09 19:08:42 -0300

    Se crea modal ver-usuario. El modfal carga los datos del usuario segun la card en la que se clickea. Los botones de cerrar de esta card funcionan correctamente.

--- js/index.js
+++ js/index.js
@@ -43,19 +43,19 @@ document.addEventListener("DOMContentLoaded", function() {
                     statusClass="inactivo"
                 }
                 contenedor.innerHTML += 
-                `<div class="main__cards">
+                `<div class="main__card" data-id="${usuario.id}">
                     <div class="card__options">
                         <div class="card__initials">
                             <span>${usuario.nombre[0]}${usuario.apellido[0]}</span>
                         </div>
                         <div class="card__icons">
-                            <button type="button" title="ver">
+                            <button type="button" title="ver" id="ver-usuario">
                                 <img src="./img/ojo.png" alt="">
                             </button>
-                            <button type="button" title="editar">
+                            <button type="button" title="editar" id="editar-usuario">
                                 <img src="./img/lapiz-de-color.png" alt="">
                             </button>
-                            <button type="button" title="eliminar">
+                            <button type="button" title="eliminar" id="eliminar-usuario">
                                 <img src="./img/tacho-de-reciclaje.png" alt="">
                             </button>
                         </div>
@@ -114,6 +114,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const inputsContainerMain = document.querySelector(".main__container-inputs");
     const main = document.querySelector(".main");
     const userContainerHeader = document.querySelector(".header__user")
+    const mainModalUsuario = document.querySelector(".main__modal-usuario")
 
     botonCancelarModal.addEventListener("click", () => {
         mainModalLogin.classList.add("hide");
@@ -183,6 +184,113 @@ document.addEventListener("DOMContentLoaded", function() {
             console.log("error");
         }
     })
+    contenedorDeCards.addEventListener("click", function(e) {
+        if(e.target.closest("#ver-usuario")){
+            const card = e.target.closest(".main__card");
+            const idUsuario = card.getAttribute("data-id")
+            const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+            const usuario = usuarios.find(u => u.id === parseInt(idUsuario));
+
+            if (usuario) {
+                cargarModalUsuario(usuario, mainModalUsuario)
+                mainModalUsuario.classList.remove("hide")
+            }
+        }
+    })
+
+function cargarModalUsuario (usuario, contenedor) {
+    contenedor.innerHTML = `
+        <div class="main__modal-usuario__portada">
+            <div class="main__modal-usuario__portada__titulo">
+                <h2>Detalle de Usuario</h2>
+                <img src="./img/cerrar.png" alt="Icono de cerrar" class="main__modal-usuario__icons cancelar">
+            </div>
+            <div class="main__modal-usuario__portada__presentacion">
+                <span class="initials initials--modal__usuario">${usuario.nombre[0]}${usuario.apellido[0]}</span>
+                <h3 class="modal__usuario-nombre">${usuario.nombre} ${usuario.apellido}</h3>
+                <p class="modal__usuario-mail__presentacion">${usuario.email}</p>
+            </div>
+            <div class="main__modal-usuario__detalles">
+                <div class="main__modal-usuario__info-personal">
+                    <h4>Informacion Personal</h4>
+                    <div class="modal-usuario__container">
+                        <div class="modal-usuario__container-img">
+                            <img src="./img/correo-electronico.png" alt="Icono de mail"class="main__modal-usuario__icons">
+                        </div>
+                        <div class="modal-usuario__container-text">
+                            <p>Email</p>
+                            <p>${usuario.email}</p>
+                        </div>
+                    </div>
+                    <div class="modal-usuario__container">
+                        <div class="modal-usuario__container-img">
+                            <img src="./img/usuario.png" alt="Icono de avatar"class="main__modal-usuario__icons">
+                        </div>
+                        <div class="modal-usuario__container-text">
+                            <p>Nombre Completo</p>
+                            <p>${usuario.nombre} ${usuario.apellido}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="main__modal-usuario__permisos-estado">
+                    <h4>Permisos y Estado</h4>
+                    <div class="modal-usuario__container">
+                        <div class="modal-usuario__container-img">
+                            <img src="./img/blindaje.png" alt="Icono de escudo"class="main__modal-usuario__icons">
+                        </div>
+                        <div class="modal-usuario__container-text">
+                            <p>Rol</p>
+                            <span>${usuario.rol}</span>
+                        </div>
+                    </div>
+                    <div class="modal-usuario__container">
+                        <div class="modal-usuario__container-img">
+                            <img src="./img/onda-de-sonido.png" alt="Icono de senal"class="main__modal-usuario__icons">
+                        </div>
+                        <div class="modal-usuario__container-text">
+                            <p>Estado</p>
+                            <span>${usuario.estado}</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="main__modal-usuario__actividad">
+                    <h4>Informacion de Actividad</h4>
+                    <div class="modal-usuario__container">
+                        <div class="modal-usuario__container-img">
+                            <img src="./img/calendario.png" alt="Icono de calendario"class="main__modal-usuario__icons">
+                        </div>
+                        <div class="modal-usuario__container-text">
+                            <p>Fecha de Creacion</p>
+                            <p>${usuario.fechaCreacion}</p>
+                        </div>
+                    </div>
+                    <div class="modal-usuario__container">
+                        <div class="modal-usuario__container-img">
+                            <img src="./img/reloj.png" alt="Icono de relopj"class="main__modal-usuario__icons">
+                        </div>
+                        <div class="modal-usuario__container-text">
+                            <p>Fecha de ultimo acceso</p>
+                            <p>${usuario.ultimoAcceso}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="main__modal-button__container">
+                <button type="reset" class="main__modal-button cancelar">
+                    Cerrar
+                </button>
+            </div>
+        </div>
+    `
+}
+    mainModalUsuario.addEventListener("click", function(e) {
+        if(e.target.closest(".main__modal-usuario__icons.cancelar")){
+
+                mainModalUsuario.classList.add("hide")
+        }
+    })
+
+
 
     inicializarUsuarios();
 })
